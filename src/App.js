d7775b6676065992f08097711b78453c01fc8c93
brain <shahriarmehedi95@gmail.com>
2021-12-15 14:28:19 +0600

    home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import Navmenu from './components/common/Navmenu'
+// import { Button } from 'bootstrap';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+
+         <>
+          <Navmenu />
+          </>
+ 
     )
   }
 }
