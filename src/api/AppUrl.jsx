80da2331bf681947d07d69e6e8862d7668aaf54e
brain <shahriarmehedi95@gmail.com>
2021-12-21 22:46:55 +0600

    show data

--- src/api/AppUrl.jsx
+++ src/api/AppUrl.jsx
@@ -1,6 +1,7 @@
 class AppUrl{
     static BaseUrl = "http://localhost:8000/api"
     static storeDetails = this.BaseUrl+"/store-details"
+    static showDetails = this.BaseUrl+"/show-details"
 
 }
 export default AppUrl
--- src/components/Updateproduct.jsx
+++ src/components/Updateproduct.jsx
@@ -1,5 +1,59 @@
-export default function Updateproduct(){
-    return(
-        <h1>Update</h1>
-    )
+import axios from 'axios';
+import React, { Component } from 'react'
+import { Container, Table } from 'react-bootstrap'
+import AppUrl from '../api/AppUrl';
+
+class Updateproduct extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            showdata:[]
+        }
+    }
+    componentDidMount(){
+        axios.get(AppUrl.showDetails).then(response=>{
+            this.setState({showdata:response.data});
+        }).catch(error=>{
+
+        })
+    }
+    render() {
+
+        const ShowData=this.state.showdata;
+        const View = ShowData.map((ShowData,i)=>{
+            return <div key={i.toString()}>
+                <tr>
+                <td>{ShowData.id}</td>
+                <td>{ShowData.email}</td>
+                <td>{ShowData.name}</td>
+                <td>{ShowData.phone}</td>
+                </tr>
+            </div>
+        });
+
+        return (
+            <>
+            <Container>
+            <Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Email</th>
+      <th>Name</th>
+      <th>Phone</th>
+    </tr>
+  </thead>
+  <tbody>
+
+            {View}
+
+  </tbody>
+</Table>
+</Container>
+            </>
+        )
+    }
 }
+
+export default Updateproduct
