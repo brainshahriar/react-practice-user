32b474b2fb10f85158a8cb8ca4e100e6a881e535
brain <shahriarmehedi95@gmail.com>
2021-12-25 19:12:25 +0600

    slik

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import Featureproduct from '../components/home/Featureproduct'
+import Newarrival from '../components/home/Newarrival'
 
 class Homepage extends Component {
     render() {
+
+        
         return (
             <>
             <Featureproduct/>
+            <Newarrival/>
             <Collection/>
             <Categories/>
             </>
